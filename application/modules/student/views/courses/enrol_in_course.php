b06cd75fd5b1cca32905882a107257b4e5eb65bd
Ankit Shukla <4nkit5hukla@gmail.com>
2021-01-30 17:37:36 +0530

    Student Module Integration From easylms

---
+++ application/modules/student/views/courses/enrol_in_course.php
@@ -0,0 +1,7 @@
+<div class="card mb-4 bg-info text-white">
+    <div class="card-body">
+        <p class="mb-3">
+            Your request for enrolment has been sent to the instructor(s). Enrolment depends on approval. Contact your coaching admin for more details.
+        </p>
+    </div>
+</div>
